58843521a8a708c307a98e50c88964cef7b03db9
Your Name <yaremiy.nazar@gmail.com>
2023-02-27 17:29:27 +0200

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,11 +16,24 @@ class App extends Component {
     ],
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     if (this.isDublicate({ name, number })) {
       return alert(`${name} is already in contacts`);
     }
+
     this.setState(prevState => {
       const newContact = {
         id: nanoid(),
